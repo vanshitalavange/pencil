209a6c12327f61e540d07f40815915bd2b73a92c
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-04-05 23:43:34 +0530

    imports and exports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { Router } from "./router/routes"
+import { Header, Footer } from "./components/index"
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router />
+      <Footer />
     </div>
   );
 }
