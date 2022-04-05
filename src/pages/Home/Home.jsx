5b2192763f3c6a54026bae8464fc0d95e67b1a0d
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-04-05 23:43:42 +0530

    feat: home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,17 @@
+import "./Home.css";
+import { Link } from "react-router-dom";
 export const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <section className="hero flex-row justify-center flex-wrap">
+      <img className="hero-banner" src="assets/banner.png" />
+      <div className="hero-desc align-center flex-column">
+        <p className="desc-heading">Pencil</p>
+        <div className="flex-column desc-subtext-wrapper">
+          <p className="desc-subtext">Pencil is a fully-functional note taking app</p>
+          <p className="desc-subtext">Keep a track of your tasks and level up your note-taking with Pencil</p>
+        </div>
+        <Link to="/signup"><button className="btn-join">Join now</button></Link>
+      </div>
+    </section>
+  );
 };
