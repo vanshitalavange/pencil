c709089b554de1479799bd2f9c48767c0f6c1d4a
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-04-05 23:43:00 +0530

    feat: footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,4 @@
+import "./Footer.css";
+export const Footer = () => {
+  return <footer className="footer text-centered">created by <a href="https://github.com/vanshitalavange/pencil"><span className="highlighted-text">vanshitalavange</span></a></footer>
+}
